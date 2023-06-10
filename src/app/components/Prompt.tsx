dc560d115c4e6c2f7d0270c503ee0a48b2894851
tlagoda <thibautlagoda@hotmail.fr>
2023-06-10 23:53:21 +0200

    modify prompt

--- src/app/components/Prompt.tsx
+++ src/app/components/Prompt.tsx
@@ -8,13 +8,13 @@ export default function Prompt() {
   return (
     <div className="mt-4 flex">
       <textarea
-        className="resize-none w-1/3 h-20 px-4 py-2 rounded-l border shadow text-sm bg-white text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        placeholder="Écrivez votre message ici..."
+        className="resize-none w-1/3 h-20 px-4 py-2 rounded-l border shadow text-sm bg-white text-gray-900 focus:outline-none"
+        placeholder="Enter your prompt here..."
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
       <button className="px-4 py-2 rounded-r bg-blue-500 text-white shadow hover:bg-blue-600">
-        Envoyer
+        Send
       </button>
     </div>
   );
