de1623a04589fc0f800cdf63a85f1e98e89137af
tlagoda <thibautlagoda@hotmail.fr>
2023-06-11 22:18:08 +0200

    cleaning

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -6,8 +6,8 @@ export default function Page() {
     <div className="h-screen w-screen bg-gray-900 font-mono text-slate-100 flex">
       <div className="w-1/4 h-full bg-gray-800">wip</div>
       <div className="w-3/4 h-full flex flex-col p-4">
-        <div className="h-full flex flex-col"> {/* make sure to add h-full here */}
-          <div className="h-3/4 flex flex-col"> {/* and here */}
+        <div className="h-full flex flex-col">
+          <div className="h-3/4 flex flex-col">
             <LinkedInPost />
           </div>
           <div className="h-1/4 flex items-center">
