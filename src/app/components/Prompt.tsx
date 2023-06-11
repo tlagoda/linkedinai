3a4cbc7ec1e3ce0a5d034bc494b17f25e05cea93
tlagoda <thibautlagoda@hotmail.fr>
2023-06-11 18:46:17 +0200

    adapt layout

--- src/app/components/Prompt.tsx
+++ src/app/components/Prompt.tsx
@@ -6,16 +6,18 @@ export default function Prompt() {
   const [text, setText] = useState("");
 
   return (
-    <div className="mt-4 flex">
-      <textarea
-        className="resize-none w-1/3 h-20 px-4 py-2 rounded-l border shadow text-sm bg-white text-gray-900 focus:outline-none"
-        placeholder="Enter your prompt here..."
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button className="px-4 py-2 rounded-r bg-blue-500 text-white shadow hover:bg-blue-600">
-        Send
-      </button>
-    </div>
+    <>
+      <div className="mt-4 flex">
+        <textarea
+          className="resize-none w-1/2 h-20 px-4 py-2 rounded-l border shadow text-sm bg-white text-gray-900 focus:outline-none"
+          placeholder="Enter your prompt here..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button className="px-4 py-2 rounded-r bg-violet-500 text-white shadow hover:bg-violet-800">
+          Send
+        </button>
+      </div>
+    </>
   );
 }
--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -4,8 +4,11 @@ export default function Page() {
   return (
     <div className="h-screen w-screen bg-gray-900 font-mono text-slate-100 flex">
       <div className="w-1/4 h-full bg-gray-800">wip</div>
-      <div className="w-3/4 h-full">
-        <Prompt />
+      <div className="w-3/4 h-full flex flex-col">
+        <div className="h-3/4"></div>
+        <div className="h-1/4">
+          <Prompt />
+        </div>
       </div>
     </div>
   );
