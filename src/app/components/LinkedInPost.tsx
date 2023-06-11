b1dbafcdd382291232e5486131f0f3c63bc288ca
tlagoda <thibautlagoda@hotmail.fr>
2023-06-11 21:58:12 +0200

    add Image next

--- src/app/components/LinkedInPost.tsx
+++ src/app/components/LinkedInPost.tsx
@@ -1,15 +1,18 @@
+import Image from "next/image";
+
 /* eslint-disable @next/next/no-img-element */
 export default function LinkedInPost() {
   return (
     <div className="h-full bg-white text-black rounded-lg w-1/2 overflow-auto shadow-md">
       <div className="p-4">
         <div className="flex items-center">
-          <img
+          <Image
             className="w-12 rounded-full mr-4"
-            src="https://media.licdn.com/dms/image/C5603AQGLNpn17Awaow/profile-displayphoto-shrink_400_400/0/1620335725641?e=1692230400&v=beta&t=wqM9h_-IeXqWRKAhy3mG_9agCiba_OoV2WKxtyggmIQ"
+            src="/pp-linkedin.jpeg"
             alt="Avatar of Thibaut Lagoda"
+            width={100}
+            height={100}
           />
-          {/* TODO Image */}
           <div>
             <p className="font-bold">Thibaut Lagoda</p>
             <p className="text-sm text-gray-600">2nd • 1h</p>
