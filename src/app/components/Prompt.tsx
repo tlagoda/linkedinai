c6ba544ef03896c78e524d7870d817b93ff336ae
tlagoda <thibautlagoda@hotmail.fr>
2023-06-11 22:16:42 +0200

    create gpt service

--- src/app/components/Prompt.tsx
+++ src/app/components/Prompt.tsx
@@ -5,6 +5,10 @@ import React, { useState } from "react";
 export default function Prompt() {
   const [text, setText] = useState("");
 
+  const generate = () => {
+    setText("");
+  };
+
   return (
     <>
       <div className="mt-4 flex w-1/2 h-3/4 bg-black">
@@ -14,7 +18,10 @@ export default function Prompt() {
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
-        <button className="px-4 py-2 w-1/6 rounded-r bg-violet-500 text-white shadow hover:bg-violet-800">
+        <button
+          onClick={generate}
+          className="px-4 py-2 w-1/6 rounded-r bg-violet-500 text-white shadow hover:bg-violet-800"
+        >
           Send
         </button>
       </div>
---
+++ src/services/gpt.service.ts
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export class GptService {
+  public async generate(prompt: string) {}
+}
