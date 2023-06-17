b2ad2155a0cb0ddb8ce6cd795c5f0efcecd19d6f
tlagoda <thibautlagoda@hotmail.fr>
2023-06-18 01:08:33 +0200

    fix btn generate height

--- src/app/components/GenerateButton.tsx
+++ src/app/components/GenerateButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const GenerateButton = () => {
   return (
-    <button className="text-2xl w-1/2 py-10 bg-violet-500 hover:bg-violet-700 text-slate-100 py-2 px-4 rounded-3xl transition-colors duration-200 flex items-center justify-center">
+    <button className="text-2xl w-1/2 h-3/5 py-10 bg-violet-500 hover:bg-violet-700 text-slate-100 py-2 px-4 rounded-3xl transition-colors duration-200 flex items-center justify-center">
       Generate!{" "}
       <span role="img" aria-label="Rocket" className="ml-2">
         🚀
