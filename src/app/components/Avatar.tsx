3c61bced29f0d8f5b831f20d2306fca687002819
tlagoda <thibautlagoda@hotmail.fr>
2023-06-17 18:10:42 +0200

    handle click outside

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, createRef, useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
 
 const Avatar = () => {
@@ -11,13 +11,32 @@ const Avatar = () => {
   const onLogout = () => {};
   const onSettings = () => {};
 
+  const menuRef = createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (isMenuOpen && !menuRef?.current?.contains(e.target as Node)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isMenuOpen, menuRef]);
+
   return (
     <div className="relative">
       <button onClick={toggleMenu}>
         <FaUserCircle size={32} />
       </button>
       {isMenuOpen && (
-        <div className="absolute top-12 right-0 mt-2 w-48 bg-white rounded-lg shadow-lg">
+        <div
+          className="absolute top-12 right-0 mt-2 w-48 bg-white rounded-lg shadow-lg"
+          ref={menuRef}
+        >
           <ul className="text-gray-600">
             <li>
               <button
