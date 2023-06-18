3a883ca0146b0bb647e906920c4998744653880f
tlagoda <thibautlagoda@hotmail.fr>
2023-06-18 02:35:14 +0200

    customSelect

--- src/app/components/optionsPanels/data.ts
+++ src/app/components/optionsPanels/data.ts
@@ -2,31 +2,31 @@ export const optionsData = [
   {
     label: "Post Topic",
     values: ["Technology", "Marketing", "Leadership", "Personal Development", "News", "Travel", "Sports", "Finance", "Fashion", "Education"],
-    defaultValue: "Technology",
+    placeholder: "Technology",
   },
   {
     label: "Tone",
     values: ["Serious", "Humorous", "Inspirational", "Persuasive", "Informative", "Controversial", "Professional", "Friendly", "Emotional", "Analytical"],
-    defaultValue: "Serious",
+    placeholder: "Serious",
   },
   {
     label: "Post Length",
     values: ["Short (100-200 characters)", "Medium (200-300 characters)", "Long (300-500 characters)", "Variable"],
-    defaultValue: "Short (100-200 characters)",
+    placeholder: "Short (100-200 characters)",
   },
   {
     label: "Target Audience",
     values: ["Industry Professionals", "Recent Graduates", "Entrepreneurs", "Students", "Domain Enthusiasts", "General Public"],
-    defaultValue: "Industry Professionals",
+    placeholder: "Industry Professionals",
   },
   {
     label: "Post Objective",
     values: ["Engage in a discussion", "Share knowledge", "Inspire others", "Promote a product/service", "Provide practical advice", "Tell a story", "Ask a question", "Share a personal experience"],
-    defaultValue: "Engage in a discussion",
+    placeholder: "Engage in a discussion",
   },
   {
     label: "Call To Action",
     values: ["Pose a question at the end of the post", "Encourage comments and shares", "Invite to visit a link", "Ask for readers' opinions", "Encourage signing up for a newsletter", "Offer additional content", "Provide a personal reflection"],
-    defaultValue: "Pose a question at the end of the post",
+    placeholder: "Pose a question at the end of the post",
   },
 ];
