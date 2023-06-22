8a706cd1573b96c52d48f5b552c84b13a3507f48
tlagoda <thibautlagoda@hotmail.fr>
2023-06-22 08:49:23 +0200

    authservice

---
+++ src/services/auth.service.ts
@@ -0,0 +1,25 @@
+export class AuthService {
+  static async signUp() {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        values.email,
+        values.password
+      );
+
+      const user = userCredential.user;
+
+      await setDoc(doc(db, "users", user.uid), {
+        email: user.email,
+        hasAuthorizedLinkedIn: false,
+        hasSubscribed: false,
+        tokensLeft: null,
+        subscription: {},
+        subscriptionPlan: null
+      });
+    } catch (error) {
+      setErrorWhileSigningUp(true);
+      console.error(error);
+    }
+  }
+}
