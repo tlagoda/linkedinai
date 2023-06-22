619f26114ba1c77bc27d8c7450628de1abd0bd29
tlagoda <thibautlagoda@hotmail.fr>
2023-06-22 08:53:45 +0200

    //

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,25 +1,25 @@
 export class AuthService {
   static async signUp() {
-    try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        values.email,
-        values.password
-      );
+    // try {
+    //   const userCredential = await createUserWithEmailAndPassword(
+    //     auth,
+    //     values.email,
+    //     values.password
+    //   );
 
-      const user = userCredential.user;
+    //   const user = userCredential.user;
 
-      await setDoc(doc(db, "users", user.uid), {
-        email: user.email,
-        hasAuthorizedLinkedIn: false,
-        hasSubscribed: false,
-        tokensLeft: null,
-        subscription: {},
-        subscriptionPlan: null
-      });
-    } catch (error) {
-      setErrorWhileSigningUp(true);
-      console.error(error);
-    }
+    //   await setDoc(doc(db, "users", user.uid), {
+    //     email: user.email,
+    //     hasAuthorizedLinkedIn: false,
+    //     hasSubscribed: false,
+    //     tokensLeft: null,
+    //     subscription: {},
+    //     subscriptionPlan: null
+    //   });
+    // } catch (error) {
+    //   setErrorWhileSigningUp(true);
+    //   console.error(error);
+    // }
   }
 }
