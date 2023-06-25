93d36f2b47cb1322c888fc398de70dbe108981b6
tlagoda <thibautlagoda@hotmail.fr>
2023-06-25 22:06:44 +0200

    edit generate btn

--- src/app/components/GenerateButton.tsx
+++ src/app/components/GenerateButton.tsx
@@ -28,7 +28,7 @@ const GenerateButton = ({
   };
   return (
     <button
-      className="text-2xl w-full h-3/5 py-10 bg-violet-500 hover:bg-violet-700 text-slate-100 py-2 px-4 rounded-3xl transition-colors duration-200 flex items-center justify-center"
+      className="text-3xl w-full h-3/5 py-10 border-violet-500 border-8 text-slate-100 py-2 px-4 rounded-3xl transition-colors duration-200 flex items-center justify-center hover:bg-purple-800 hover:text-white hover:border-purple-800"
       onClick={generatePost}
     >
       Generate!{" "}
---
+++ src/app/components/PublishSection.tsx
@@ -0,0 +1,5 @@
+export default function PublishSection() {
+  return (
+    <div className="flex items-center justify-between text-black w-4/5 pt-5 mx-auto h-20"></div>
+  );
+}
