63500c40bd6a3b13f184605eb57ce2c4e5d3e5a9
tlagoda <thibautlagoda@hotmail.fr>
2023-06-27 15:24:10 +0200

    adapt generate service

--- src/services/gpt.service.ts
+++ src/services/gpt.service.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { config } from "./config/config";
+import { getAuth } from "firebase/auth";
+import "firebase/auth";
 
 export class GptService {
   static async generate(prompt: string) {
@@ -8,13 +10,22 @@ export class GptService {
         ? config.production.apiUrl
         : config.development.apiUrl;
     const apiUrl = `${baseApiUrl}/posts/generate`;
+
+    const auth = getAuth();
+    const token = await auth.currentUser?.getIdToken(); // firebase auto mangaes cache
+
     try {
       const response = await axios.get(apiUrl, {
         params: {
           prompt,
         },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       });
-      return response.data.text;
+
+      return response.data.message;
+      // return response.data.text;
     } catch (error) {
       console.error(`Error generating post: ${error}`);
     }
