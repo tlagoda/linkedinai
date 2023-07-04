0bb4b03680247ac31de16430a000f172f9caea1a
tlagoda <thibautlagoda@hotmail.fr>
2023-07-04 22:18:23 +0200

    style

--- src/app/components/Prompt.tsx
+++ src/app/components/Prompt.tsx
@@ -37,7 +37,7 @@ export default function Prompt({
     <>
       <div className="mt-4 flex flex-col md:flex-row md:w-3/5 mx-auto h-3/4 mb-4 md:mb-0">
         <textarea
-          className="resize-none w-full md:w-8/12 h-40 md:h-full px-4 py-2 rounded-l border-2 border-gray-800 shadow text-sm bg-white text-gray-900 focus:outline-none"
+          className="resize-none w-full md:w-8/12 h-40 md:h-full px-4 py-2 rounded-t border-b-0 md:rounded-t-none md:rounded-l border-2 md:border-r-0 md:border-b-2 border-black shadow text-sm bg-slate-700 text-slate-100 focus:outline-none placeholder-slate-100"
           placeholder="Enter your prompt here..."
           value={text}
           onChange={(e) => setText(e.target.value)}
@@ -45,7 +45,7 @@ export default function Prompt({
         <div className="flex flex-row md:flex-col md:w-4/12">
           <button
             onClick={generatePost}
-            className="px-4 py-4 md:py-2 w-full h-1/2 rounded-tr bg-blue-500 border-2 border-gray-800 text-white shadow hover:bg-blue-800 text-sm transition-colors duration-300 ease-in-out"
+            className="px-4 py-4 md:py-2 w-full h-1/2 rounded-bl md:rounded-tr md:rounded-bl-none bg-emerald-400 text-black border-r-0 md:border-r-2 border-2 md:border-b-0 border-black font-bold shadow hover:bg-emerald-600 text-sm transition-colors duration-300 ease-in-out"
           >
             Generate
             <span
@@ -58,7 +58,7 @@ export default function Prompt({
           </button>
           <button
             onClick={generatePost}
-            className="px-4 py-4 md:py-2 w-full h-1/2 rounded-br bg-blue-500 border-2 border-gray-800 text-white shadow hover:bg-blue-800 text-sm transition-colors duration-300 ease-in-out"
+            className="px-4 py-4 md:py-2 w-full h-1/2 rounded-br bg-emerald-400 border-2 border-black text-black font-bold shadow hover:bg-emerald-600 text-sm transition-colors duration-300 ease-in-out"
           >
             Publish
             <span
