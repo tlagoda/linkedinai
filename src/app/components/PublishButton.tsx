1c2c65ac515a43823632fee51ec32ff9baf51218
tlagoda <thibautlagoda@hotmail.fr>
2023-07-04 22:04:57 +0200

    edit btns

--- src/app/components/PublishButton.tsx
+++ src/app/components/PublishButton.tsx
@@ -29,7 +29,7 @@ export default function PublishButton({
     <>
       <button
         onClick={handlePublish}
-        className="text-xl font-bold bg-orange-300 md:bg-transparent text-black w-full my-8 md:my-0 md:w-1/2  mx-auto h-3/5 py-4 md:py-10 border-orange-300 border-4 md:text-slate-100 px-4 rounded-xl transition-colors duration-200 flex items-center justify-center hover:bg-orange-300 hover:text-black hover:border-orange-300"
+        className="text-xl font-bold bg-emerald-400 md:bg-transparent text-black w-2/3 my-8 md:my-0 md:w-1/2  mx-auto h-3/5 py-4 md:py-10 border-black md:border-emerald-400 border-4 md:text-slate-100 px-4 rounded-xl transition-colors duration-200 flex items-center justify-center hover:bg-emerald-400 hover:text-black hover:border-emerald-400"
       >
         Publish!{" "}
         <span role="img" aria-label="Rocket" className="ml-2 animate-bounce">
