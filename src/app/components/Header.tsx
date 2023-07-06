b875e995178323f3c998952f3f63a858de4dd8e5
tlagoda <thibautlagoda@hotmail.fr>
2023-07-06 17:07:06 +0200

    header mobile

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import CallToActionLink from "./CallToActionLink";
 import { FaBars } from "react-icons/fa";
 import Link from "next/link";
+import {HorizontalDivider} from "../components/HorizontalDivider"
 
 interface HeaderProps {
   title?: string;
@@ -16,6 +17,7 @@ export default function Header({
 }: HeaderProps) {
   const CTALink = "/signup";
   const [tailwindMd, setTailwindMd] = useState<boolean | undefined>(undefined);
+  const [showMenu, setShowMenu] = useState<boolean>(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -30,37 +32,67 @@ export default function Header({
   }, []);
 
   return (
-    <div className="flex items-center justify-between mx-4 md:mx-16">
-      <div
-        className={!tailwindMd ? "flex w-1/2 justify-around items-center" : ""}
-      >
-        {!tailwindMd && <FaBars className="text-2xl text-emerald-400" />}
-        <h1 className="text-2xl md:text-3xl text-slate-100 font-bold hover:cursor-pointer">
-          {!title && (
-            <Link href="/">
-              l<span className="text-blue-500">AI</span>nkedIn
-            </Link>
+    <>
+      <div className="w-screen h-14 pt-4 px-4 bg-gray-900 z-20 relative">
+        <div className="flex items-center justify-between md:mx-16 z-20 bg-gray-900">
+          <div
+            className={
+              !tailwindMd ? "flex w-1/2 justify-around items-center" : ""
+            }
+          >
+            {!tailwindMd && (
+              <FaBars
+                onClick={() => setShowMenu(!showMenu)}
+                className="text-2xl text-emerald-400 cursor-pointer"
+              />
+            )}
+            <h1 className="text-2xl md:text-3xl text-slate-100 font-bold hover:cursor-pointer">
+              {!title && (
+                <Link href="/">
+                  l<span className="text-blue-500">AI</span>nkedIn
+                </Link>
+              )}
+              {title && title}
+            </h1>
+          </div>
+          {tailwindMd && (
+            <ul className="text-slate-100 text-xl flex justify-evenly items-center w-6/12">
+              <li>|</li>
+              {menuItems.map((item, index) => {
+                return (
+                  <li
+                    className="hover:underline hover:cursor-pointer hover:underline-offset-4 hover:text-emerald-400"
+                    key={index}
+                  >
+                    {item}
+                  </li>
+                );
+              })}
+              <li>|</li>
+            </ul>
           )}
-          {title && title}
-        </h1>
+          <CallToActionLink content={CTATitle} href={CTALink} />
+        </div>
       </div>
-      {tailwindMd && (
-        <ul className="text-slate-100 text-xl flex justify-evenly items-center w-6/12">
-          <li>|</li>
-          {menuItems.map((item, index) => {
-            return (
-              <li
-                className="hover:underline hover:cursor-pointer hover:underline-offset-4 hover:text-emerald-400"
-                key={index}
-              >
-                {item}
-              </li>
-            );
-          })}
-          <li>|</li>
-        </ul>
+      {!tailwindMd && showMenu && (
+        <div className="absolute bg-gray-900 top-14 left-0 w-screen p-4 text-emerald-400 z-20">
+          <HorizontalDivider />
+          {menuItems.map((item, index) => (
+            <div
+              key={index}
+              className="py-2 hover:text-emerald-400 cursor-pointer"
+            >
+              {item}
+            </div>
+          ))}
+        </div>
       )}
-      <CallToActionLink content={CTATitle} href={CTALink} />
-    </div>
+      {showMenu && (
+        <div
+          className="absolute top-0 left-O inset-0 bg-black opacity-50 z-10"
+          onClick={() => setShowMenu(false)}
+        ></div>
+      )}
+    </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,49 +26,17 @@ export default function Home() {
 
   return (
     <main>
-      <div className="min-h-screen font-mono w-screen">
-        <div className="md:h-screen w-screen bg-gray-900 pt-4 pb-10 md:pb-0">
-          <Header />
-          <div className="flex flex-col md:flex-row items-center mt-20 md:mx-20">
-            <div className="w-11/12 md:w-6/12 px-8 flex flex-col items-center">
-              <h2 className="text-5xl md:text-8xl text-slate-100 text-center md:mb-10">
-                Worry Less, Inspire More
-              </h2>
-              {!tailwindMd && (
-                <div className="w-11/12">
-                  <Image
-                    className="my-10"
-                    alt="screenshot"
-                    src="/screen-banner.png"
-                    width={700}
-                    height={360}
-                  />
-                </div>
-              )}
-              <p className="text-l text-slate-300 text-center">
-                Unleash the power of{" "}
-                <span className="text-emerald-400">AI</span> with our
-                next-generation LinkedIn posting tool. Craft{" "}
-                <span className="text-emerald-400">
-                  engaging, top-quality content
-                </span>{" "}
-                and share it directly to your network with ease. Our
-                sophisticated algorithms turn your ideas into compelling
-                narratives, making professional networking{" "}
-                <span className="text-emerald-400">more effective</span> than
-                ever.
-                <br />
-                <br />
-                <span className="text-emerald-400">Stand out</span> from the
-                crowd, shape the conversation, and elevate your{" "}
-                <span className="text-emerald-400">LinkedIn presence</span> with
-                our unique SaaS solution.
-              </p>
-            </div>
-            {tailwindMd && (
-              <div className="w-6/12">
+      <div className="md:h-screen w-screen bg-gray-900 pb-10 md:pb-0 min-h-screen font-mono w-screen ">
+        <Header />
+        <div className="flex flex-col md:flex-row items-center py-8 md:mx-20">
+          <div className="w-11/12 md:w-6/12 px-8 flex flex-col items-center md:mt-20">
+            <h2 className="text-5xl md:text-8xl text-slate-100 text-center md:mb-10">
+              Worry Less, Inspire More
+            </h2>
+            {!tailwindMd && (
+              <div className="w-full  ">
                 <Image
-                  className="mx-auto"
+                  className="my-10"
                   alt="screenshot"
                   src="/screen-banner.png"
                   width={700}
@@ -76,10 +44,39 @@ export default function Home() {
                 />
               </div>
             )}
+            <p className="text-l text-slate-300 text-center">
+              Unleash the power of <span className="text-emerald-400">AI</span>{" "}
+              with our next-generation LinkedIn posting tool. Craft{" "}
+              <span className="text-emerald-400">
+                engaging, top-quality content
+              </span>{" "}
+              and share it directly to your network with ease. Our sophisticated
+              algorithms turn your ideas into compelling narratives, making
+              professional networking{" "}
+              <span className="text-emerald-400">more effective</span> than
+              ever.
+              <br />
+              <br />
+              <span className="text-emerald-400">Stand out</span> from the
+              crowd, shape the conversation, and elevate your{" "}
+              <span className="text-emerald-400">LinkedIn presence</span> with
+              our unique SaaS solution.
+            </p>
           </div>
-          <div className="flex justify-center mt-20">
-            <CallToActionLink content="Try it now!" href={CTALink} />
-          </div>
+          {tailwindMd && (
+            <div className="w-6/12">
+              <Image
+                className="mx-auto"
+                alt="screenshot"
+                src="/screen-banner.png"
+                width={700}
+                height={360}
+              />
+            </div>
+          )}
+        </div>
+        <div className="flex justify-center mt-20">
+          <CallToActionLink content="Try it now!" href={CTALink} />
         </div>
       </div>
     </main>
