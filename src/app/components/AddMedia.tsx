5ab46d545d37f6b62819946e714b82ceacd36039
tlagoda <thibautlagoda@hotmail.fr>
2023-07-07 20:59:17 +0200

    input file

--- src/app/components/AddMedia.tsx
+++ src/app/components/AddMedia.tsx
@@ -5,12 +5,14 @@ export default function AddMedia() {
     <div className="flex flex-col justify-between">
       <h3 className="text-center">Add media:</h3>
       <div className="w-3/5 flex  mx-auto mt-4">
-        <button className="w-1/2 p-2 rounded-l flex border border-emerald-400 justify-center items-center hover:bg-emerald-400 hover:text-black">
+        <label className="w-1/2 p-2 rounded-l flex border border-emerald-400 justify-center items-center hover:bg-emerald-400 hover:text-black">
           Image <FaImage className="ml-2"/>
-        </button>
-        <button className="w-1/2 p-2 rounded-r flex border-y border-r border-emerald-400 justify-center items-center hover:bg-emerald-400 hover:text-black">
+          <input type="file" accept="image/*" style={{ display: "none" }} id="imageInput" />
+        </label>
+        <label className="w-1/2 p-2 rounded-r flex border-y border-r border-emerald-400 justify-center items-center hover:bg-emerald-400 hover:text-black">
           Video <FaVideo className="ml-2"/>
-        </button>
+          <input type="file" accept="video/*" style={{ display: "none" }} id="videoInput" />
+        </label>
       </div>
     </div>
   );
