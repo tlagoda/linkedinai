0893d1a9db66f7c21a40f2f6d008a7ec24fc7972
tlagoda <thibautlagoda@hotmail.fr>
2023-07-07 21:08:40 +0200

    handle upload image/video

--- src/app/components/AddMedia.tsx
+++ src/app/components/AddMedia.tsx
@@ -1,17 +1,43 @@
+import { useState } from "react";
 import { FaImage, FaVideo } from "react-icons/fa";
 
 export default function AddMedia() {
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const [selectedVideo, setSelectedVideo] = useState<File | null>(null);
+
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    setSelectedImage(file || null);
+  };
+
+  const handleVideoUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    setSelectedVideo(file || null);
+  };
+
   return (
     <div className="flex flex-col justify-between">
       <h3 className="text-center">Add media:</h3>
       <div className="w-3/5 flex  mx-auto mt-4">
         <label className="w-1/2 p-2 rounded-l flex border border-emerald-400 justify-center items-center hover:bg-emerald-400 hover:text-black">
-          Image <FaImage className="ml-2"/>
-          <input type="file" accept="image/*" style={{ display: "none" }} id="imageInput" />
+          Image <FaImage className="ml-2" />
+          <input
+            type="file"
+            accept="image/*"
+            style={{ display: "none" }}
+            id="imageInput"
+            onChange={handleImageUpload}
+          />
         </label>
         <label className="w-1/2 p-2 rounded-r flex border-y border-r border-emerald-400 justify-center items-center hover:bg-emerald-400 hover:text-black">
-          Video <FaVideo className="ml-2"/>
-          <input type="file" accept="video/*" style={{ display: "none" }} id="videoInput" />
+          Video <FaVideo className="ml-2" />
+          <input
+            type="file"
+            accept="video/*"
+            style={{ display: "none" }}
+            id="videoInput"
+            onChange={handleVideoUpload}
+          />
         </label>
       </div>
     </div>
