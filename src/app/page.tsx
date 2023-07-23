e91f407c5bc09e6bb56e4999d2bc2727509e0ddf
tlagoda <thibautlagoda@hotmail.fr>
2023-07-23 18:38:23 +0200

    creativity section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Header />
       <div className="bg-myblue-500 text-slate-100 min-h-screen pb-20">
         <div className="flex my-10 w-2/3 mx-auto">
-          <div className="w-1/2 mx-10">
+          <div className="w-1/2 mx-10 mx-10 flex flex-col justify-center">
             <h3 className="text-6xl mb-10 font-bold">
               Unlock Limitless LinkedIn Potential with Kouim
             </h3>
@@ -53,6 +53,26 @@ export default function Home() {
           </div>
           <div className="w-1/2 mx-10 flex flex-col justify-center">
             <h3 className="text-6xl mb-10 font-bold">
+              Boost Creativity and Enhance Your Online Presence!
+            </h3>
+            <p className="mb-10">
+              Kouim is your ultimate creative companion for LinkedIn. With this
+              ultimate LinkedIn post generator, you will discover new ways to
+              express yourself and captivate your audience. Elevate your online
+              presence and stand out in the crowd. Join now and experience the
+              power of limitless creativity on LinkedIn!
+            </p>
+            <button className="w-1/3 bg-emerald-400 rounded-full py-4 hover:bg-emerald-500">
+              Create!
+            </button>
+          </div>
+        </div>
+        <div className="h-40 bg-blue-100">
+
+        </div>
+        <div className="flex my-10 w-2/3 mx-auto my-40">
+          <div className="w-1/2 mx-10 flex flex-col justify-center">
+            <h3 className="text-6xl mb-10 font-bold">
               Achieve More in Less Time with Kouim{" "}
             </h3>
             <p className="mb-10">
@@ -63,6 +83,15 @@ export default function Home() {
             <button className="w-1/3 bg-emerald-400 rounded-full py-4 hover:bg-emerald-500">
               Save time!
             </button>
+          </div>
+          <div className="w-1/2 flex justify-center items-center">
+            <Image
+              className="rounded-xl border-2 border-emerald-400"
+              src="/time-saver.png"
+              alt="Landing Image"
+              width={450}
+              height={450}
+            />
           </div>
         </div>
       </div>
