7b3a3209a59519792cab2588ada639c7fe26e4d8
tlagoda <thibautlagoda@hotmail.fr>
2023-07-23 16:04:33 +0200

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,38 @@
 "use client";
 
 import Header from "./components/Header";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-myblue-500">
       <Header />
       <div className="bg-myblue-500 text-slate-100 min-h-screen">
-        <div className="flex">
-          <div className="w-1/2">
-            <h3 className="text-6xl font-bold">Unlock Limitless LinkedIn Potential with Kouim</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quasi,
-              minus odio rerum dolor tenetur laudantium veniam labore esse,
-              impedit ad modi nesciunt aut laborum quibusdam corrupti cum vel
-              eum.
+        <div className="flex my-10 w-2/3 mx-auto">
+          <div className="w-1/2 mx-10">
+            <h3 className="text-6xl mb-10 font-bold">
+              Unlock Limitless LinkedIn Potential with Kouim
+            </h3>
+            <p className="mb-10">
+              Unlock your LinkedIn potential with Kouim, the AI-powered post
+              generator. Create custom posts, add media, and effortlessly
+              publish on LinkedIn.
             </p>
+            <button className="w-1/3 bg-emerald-400 rounded-full py-4 hover:bg-emerald-500">
+              Try for free!
+            </button>
+          </div>
+          <div className="w-1/2 flex justify-center items-center">
+            <Image
+              className="rounded-xl"
+              src="/landing-1.png"
+              alt="Landing Image"
+              width={450}
+              height={450}
+            />
           </div>
         </div>
+        <div className="w-full h-40 bg-red-400"></div>
       </div>
     </main>
   );
