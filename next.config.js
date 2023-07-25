7d4ba521cca08408457efe828a7f7cef0d066d91
tlagoda <thibautlagoda@hotmail.fr>
2023-07-25 18:30:28 +0200

    add allowed domain for images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["media.licdn.com", "miro.medium.com"],
+    domains: ["media.licdn.com", "miro.medium.com", "pbs.twimg.com"],
   },
 };
 
